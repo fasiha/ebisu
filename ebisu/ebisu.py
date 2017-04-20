10368ec3d23c93cf7d4230218fdad6878e61d4b5
Ahmed Fasih <wuzzyview@gmail.com>
2017-04-19 22:11:32 -0400

    gammaln for more accuracy. Setup.py

--- ebisu/ebisu.py
+++ ebisu/ebisu.py
@@ -1,7 +1,7 @@
 def recallProbabilityMean(alpha, beta, t, tnow):
   # `Integrate[p^((a - t)/t) * (1 - p^(1/t))^(b - 1) * (p)/t/(Gamma[a]*Gamma[b]/Gamma[a+b]), {p, 0, 1}]`
   from scipy.special import gammaln
-  from math import exp
+  from numpy import exp
   dt = tnow / t
   return exp(
       gammaln(alpha + dt) - gammaln(alpha + beta + dt) - (
@@ -9,21 +9,22 @@ def recallProbabilityMean(alpha, beta, t, tnow):
 
 
 def recallProbabilityVar(alpha, beta, t, tnow):
-  from scipy.special import gamma
+  from numpy import exp
+  from scipy.special import gammaln
   # `Assuming[a>0 && b>0 && t>0, {Integrate[p^((a - t)/t) * (1 - p^(1/t))^(b - 1) * (p-m)^2/t/(Gamma[a]*Gamma[b]/Gamma[a+b]), {p, 0, 1}]}]``
   # And then plug in mean for `m` & simplify to get:
   dt = tnow / t
-  same0 = gamma(alpha) / gamma(alpha + beta)
-  same1 = gamma(alpha + dt) / gamma(alpha + beta + dt)
-  same2 = gamma(alpha + 2 * dt) / gamma(alpha + beta + 2 * dt)
-  md = same1 / same0
-  md2 = same2 / same0
-  return md2 - md**2
+  same0 = gammaln(alpha) - gammaln(alpha + beta)
+  same1 = gammaln(alpha + dt) - gammaln(alpha + beta + dt)
+  same2 = gammaln(alpha + 2 * dt) - gammaln(alpha + beta + 2 * dt)
+  md = same1 - same0
+  md2 = same2 - same0
+  return exp(md2) - exp(2 * md2)
 
 
 def posteriorAnalytic(alpha, beta, t, result, tnow):
   from scipy.special import gammaln, gamma
-  from math import exp
+  from numpy import exp
   dt = tnow / t
   if result == 1:
     # marginal: `Integrate[p^((a - t)/t)*(1 - p^(1/t))^(b - 1)*p, {p,0,1}]`
@@ -31,9 +32,10 @@ def posteriorAnalytic(alpha, beta, t, result, tnow):
     # variance: `Integrate[p^((a - t)/t)*(1 - p^(1/t))^(b - 1)*p*(p - m)^2, {p,0,1}]`
     # Simplify all three to get the following:
     same = gammaln(alpha + beta + dt) - gammaln(alpha + dt)
-    mu = exp(gammaln(alpha + 2 * dt) - gammaln(alpha + beta + 2 * dt) + same)
+    muln = gammaln(alpha + 2 * dt) - gammaln(alpha + beta + 2 * dt) + same
+    mu = exp(muln)
     var = exp(same + gammaln(alpha + 3 * dt) -
-              gammaln(alpha + beta + 3 * dt)) - mu**2
+              gammaln(alpha + beta + 3 * dt)) - exp(2 * muln)
   else:
     # Mathematica code is same as above, but replace one `p` with `(1-p)`
     # marginal: `Integrate[p^((a - t)/t)*(1 - p^(1/t))^(b - 1)*(1-p), {p,0,1}]`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ebisu',
+      version='0.1',
+      description='Intelligent quiz scheduling',
+      url='http://github.com/fasiha/ebisu',
+      author='Ahmed Fasih',
+      author_email='wuzzyview@gmail.com',
+      license='Unlicense',
+      packages=['ebisu'],
+      zip_safe=True)

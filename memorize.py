f35aba92dc29be995e7c2f4d8b1fa809102f2038
Ahmed Fasih <wuzzyview@gmail.com>
2019-11-06 22:50:43 -0500

    No-numpy self-contained documented memorize algorithm

--- memorize.py
+++ memorize.py
@@ -1,36 +1,63 @@
-from typing import Callable
-import numpy as np
+from random import random
+from math import sqrt, log
+from typing import Callable  # just for static type checks
+
+
+def exponential(scale: float) -> float:
+  """Replacement for numpy.random.exponential"""
+  return -log(random()) * scale
 
 
 def intensity(t: float, timeToRecallProb: Callable[[float], float], q: float) -> float:
+  """Evaluate the review intensity u(t)
+
+  Inputs:
+  - `t` time units in the future
+  - `timeToRecallProb` function that returns recall probability at any given time
+  - `q` tunable parameter balancing forgetting versus high review rates
+
+  Returns a Poisson process' intensity.
+  """
   p = timeToRecallProb(t)
   assert p >= 0 and p <= 1
-  return 1.0 / np.sqrt(q) * (1 - p)
+  return 1.0 / sqrt(q) * (1 - p)
 
 
 def sampler(timeToRecallProb: Callable[[float], float], q: float, T: float):
+  """Random draw from the thinned Poisson point process
+
+  Inputs:
+  - `timeToRecallProb` function that returns recall probability at any given time
+  - `q` tunable parameter balancing forgetting versus high review rates
+  - `T` maximum time horizon
+
+  Returns a time in the future to schedule this item *or* `None` if the item should not be
+  scheduled within `T` time units.
+  """
   t = 0
-  max_int = 1.0 / np.sqrt(q)
+  max_int = 1.0 / sqrt(q)
   max_int_inv = 1.0 / max_int
   while (True):
-    t_ = np.random.exponential(max_int_inv)
+    t_ = exponential(max_int_inv)
     if t_ + t > T:
       return None
     t = t + t_
     proposed_int = intensity(t, timeToRecallProb, q)
-    if np.random.uniform(0, 1, 1)[0] < (proposed_int * max_int_inv):
+    if random() < (proposed_int * max_int_inv):
       return t
 
 
 if __name__ == '__main__':
-  import ebisu
   model = [3., 3., 5.5]
   # 0 probability of recall -> sqrt(q) reviews per unit time
   q = 1e-2
   T = 1000.
+
+  mean = lambda v: sum(v) / len(v)
+  import ebisu
   res = [sampler(lambda t: ebisu.predictRecall(model, t + 0.0, True), q, T) for i in range(1000)]
-  print('mean', np.mean(res))
+  print('mean', mean(res))
   res = [sampler(lambda t: ebisu.predictRecall(model, t + 1.0, True), q, T) for i in range(1000)]
-  print('mean', np.mean(res))
+  print('mean', mean(res))
   res = [sampler(lambda t: ebisu.predictRecall(model, t + 22.0, True), q, T) for i in range(1000)]
-  print('mean', np.mean(res))
+  print('mean', mean(res))

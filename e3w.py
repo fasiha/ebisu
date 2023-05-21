b464f3d7b0d7e5f25b3edf3ee7b30d3147f4b528
Ahmed Fasih <wuzzyview@gmail.com>
2023-05-21 04:03:32 -0700

    horrible bugfix in update, in expm1; with weibull analytical WIP

--- e3w.py
+++ e3w.py
@@ -1,3 +1,6 @@
+import mpmath as mp
+import sympy as sp
+from sympy import S
 from pprint import pprint
 from scipy.special import gammaln
 from ebisu import logsumexp
@@ -47,6 +50,113 @@ class Model():
   weights: Optional[Any] = None
 
 
+def normLogW(logWeights):
+  ws = np.exp(logWeights)
+  return ws / sum(ws)
+
+
+def predict(model: Model, hoursElapsed: float) -> float:
+  logps = (-hoursElapsed / model.samples) * np.log(2)
+  ws = model.weights if model.weights is not None else 1 / len(model.logWeights)
+  return sum(ws * np.exp(logps))
+
+
+def fitWeibull(model: Model):
+  m1 = sum(model.weights * model.samples)
+  m2 = sum(model.weights * model.samples**2)
+  from scipy.special import gamma
+  resK = minimize_scalar(
+      lambda k: abs(m2 - (m1 / gamma(1 + 1 / k))**2 * gamma(1 + 2 / k)),
+      bracket=[.1, 1],
+      method='golden')
+  resLambda = m1 / gamma(1 + 1 / resK.x)
+  return (resK.x, resLambda, weibull_min(resK.x, scale=resLambda))
+
+
+def update(model: Model, hoursElapsed: float, correct: int | bool) -> Model:
+  logps = (-hoursElapsed / model.samples) * np.log(2)
+  model.logWeights += logps if correct else np.log(-np.expm1(logps))
+  model.weights = normLogW(model.logWeights)
+  return model
+
+
+def halflife(model: Model) -> float:
+  ws = model.weights if model.weights is not None else 1 / len(model.logWeights)
+  res = minimize_scalar(
+      lambda h: abs(0.5 - np.sum(ws * np.exp2(-h / model.samples))),
+      bracket=[1, 1000],
+      method='golden',
+      tol=.1,
+      options=dict(maxiter=50))
+  return res.x
+
+
+@dataclass
+class WeibullRational():
+  p: Any
+  q: Any
+  l: Any
+
+  def evalExpectation(self, t: float, n: int) -> float:
+    p, q, l = self.p, self.q, self.l
+    prefix = (t / p)**(p / q + n) * np.sqrt(p / q) / l**(p / q) / np.sqrt(2 * np.pi)**(p + q - 2)
+    arg = (t / l / p)**p / q**q
+    meijerList = [(x - (p / q) - n) / p for x in range(1, p)] + [-1 / q - n / p
+                                                                ] + [x / q for x in range(int(q))]
+    return float(prefix) * float(mp.meijerg([[], []], [meijerList, []], arg))
+
+  def predict(self, hoursElapsed: float):
+    return self.evalExpectation(t=hoursElapsed * np.log(2), n=0)
+
+  def update(self, hoursElapsed: float, correct: int | bool):
+    """
+    success:
+
+    posterior = (PWeibull(X) * exp(-t/X)) / (int(PWeibull(X) * exp(-t/X)))
+
+    posterior mean = int( X (PWeibull(X) * exp(-t/X)) / (int(PWeibull(X) * exp(-t/X))), x, 0, ∞)
+      = evalExpectation(n=1) / evalExpectation(n=0)
+    
+    posterior m2 = int( X^2 (PWeibull(X) * exp(-t/X)) / (int(PWeibull(X) * exp(-t/X))), x, 0, ∞)
+      = evalExpectation(n=2) / evalExpectation(n=0)
+
+    failure:
+
+    posterior = (PWeibull(X) * (1 - exp(-t/X))) / (int(PWeibull(X) * (1 - exp(-t/X))))
+      = sum([
+        PWeibull(X),
+        -(PWeibull(X) * exp(-t/X)),
+      ]) / sum([
+        int(PWeibull(X), x, 0, ∞),
+        -int(PWeibull(X) * exp(-t/X), x, 0, ∞)
+      ])
+      = numerator / denominator
+
+      denominator = 1 - evalExpectation(n=0)
+    
+      posterior mean = int(X * Posterior(X))
+        = int(X*PWeibull(X) - X*PWeibull(X)*exp(-t/X)) / denominator
+        = (E[X] - evalExpectation(n=1)) / denominator
+      posterior m2 = int(X^2, Posterior(X))
+        = (E[X^2] - evalExpectation(n=2)) / denominator
+    """
+    t = hoursElapsed * np.log(2)
+    if correct:
+      den = self.evalExpectation(t=t, n=0)
+      m1 = self.evalExpectation(t=t, n=1) / den
+      m2 = self.evalExpectation(t=t, n=2) / den
+      return (m1, m2)
+
+    den = 1 - self.evalExpectation(t=t, n=0)
+    wrv = weibull_min(self.p / self.q, scale=self.l)
+    wm1 = wrv.mean()  # prior mean
+    wm2 = wrv.moment(2)  # prior 2nd moment
+
+    m1 = (wm1 - self.evalExpectation(t=t, n=1)) / den
+    m2 = (wm2 - self.evalExpectation(t=t, n=2)) / den
+    return (m1, m2)
+
+
 size = 1_000_000
 # a,b = 1.0, 1.0 # loosest, probably will be unstable
 a, b = 2.0, 2.0  # looser
@@ -59,10 +169,28 @@ mu, s2 = np.log(10), 2
 hlSamplesLnorm = lognorm.rvs(np.sqrt(s2), scale=np.exp(mu), size=size)
 logNorm = Model(logWeights=np.zeros(size), samples=hlSamplesLnorm)
 
-k, l = .25, 1
+kp, kq = 1, 4
+k, l = kp / kq, 1
 wrv = weibull_min(k, scale=l)
 hlSamplesWeibull = wrv.rvs(size)
 weibully = Model(logWeights=np.zeros(size), samples=hlSamplesWeibull)
+weibullModel = WeibullRational(kp, kq, l)
+
+confirmMeijer = False
+if confirmMeijer:
+  print('PRED: actual', weibullModel.predict(10.), 'expected', predict(weibully, 10.))
+  act0 = weibullModel.update(10, 0)
+  u0 = update(weibully, 10, 0)
+  exp0 = (np.sum(weibully.weights * weibully.samples),
+          np.sum(weibully.weights * weibully.samples**2))
+  print(act0, exp0)
+
+  weibully = Model(logWeights=np.zeros(size), samples=hlSamplesWeibull)
+  act1 = weibullModel.update(10, 1)
+  u1 = update(weibully, 10, 1)
+  exp1 = (np.sum(weibully.weights * weibully.samples),
+          np.sum(weibully.weights * weibully.samples**2))
+  print(act1, exp1)
 
 
 def sampleBoundedPareto(lo: float, hi: float, a: float, size: int):
@@ -104,48 +232,6 @@ plt.gca().set_xscale('log')
 plt.gca().set_yscale('log')
 plt.xlabel('hours')
 plt.legend()
-
-
-def normLogW(logWeights):
-  ws = np.exp(logWeights)
-  return ws / sum(ws)
-
-
-def predict(model: Model, hoursElapsed: float) -> float:
-  logps = (-hoursElapsed / model.samples) * np.log(2)
-  ws = model.weights if model.weights is not None else 1 / len(model.logWeights)
-  return sum(ws * np.exp(logps))
-
-
-def fitWeibull(model: Model):
-  m1 = sum(model.weights * model.samples)
-  m2 = sum(model.weights * model.samples**2)
-  from scipy.special import gamma
-  resK = minimize_scalar(
-      lambda k: abs(m2 - (m1 / gamma(1 + 1 / k))**2 * gamma(1 + 2 / k)),
-      bracket=[.1, 1],
-      method='golden')
-  resLambda = m1 / gamma(1 + 1 / resK.x)
-  return (resK.x, resLambda, weibull_min(resK.x, scale=resLambda))
-
-
-def update(model: Model, hoursElapsed: float, correct: int | bool) -> Model:
-  logps = (-hoursElapsed / model.samples) * np.log(2)
-  model.logWeights += logps if correct else np.expm1(-np.expm1(logps))
-  model.weights = normLogW(model.logWeights)
-  return model
-
-
-def halflife(model: Model) -> float:
-  ws = model.weights if model.weights is not None else 1 / len(model.logWeights)
-  res = minimize_scalar(
-      lambda h: abs(0.5 - np.sum(ws * np.exp2(-h / model.samples))),
-      bracket=[1, 1000],
-      method='golden',
-      tol=.1,
-      options=dict(maxiter=50))
-  return res.x
-
 
 now = 0
 eb = ebisu.initModel(halflife=10 * 10, now=now, power=14, n=8, firstHalflife=7.5)

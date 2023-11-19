4b94c8611a3e51d1c8c0bf94a48ab69ba7e4abcd
Ahmed Fasih <wuzzyview@gmail.com>
2023-11-19 14:53:24 -0800

    anki history analyzer, for comparing to beta-powerlaw

---
+++ scripts/analyzeHistory.py
@@ -0,0 +1,149 @@
+import ebisu
+
+from tqdm import tqdm  #type:ignore
+import numpy as np
+import pylab as plt  # type:ignore
+import typing
+import utils
+from scipy.stats import binom as binomrv  # type:ignore
+from math import log
+
+plt.style.use('ggplot')
+plt.rcParams['svg.fonttype'] = 'none'
+plt.ion()
+
+ConvertAnkiMode = typing.Literal['approx', 'binary']
+MILLISECONDS_PER_HOUR = 3600e3  # 60 min/hour * 60 sec/min * 1e3 ms/sec
+
+
+def convertAnkiResultToBinomial(result: int, mode: ConvertAnkiMode) -> dict:
+  if mode == 'approx':
+    # Try to approximate hard to easy with binomial: this is tricky and ad hoc
+    if result == 1:  # fail
+      return dict(successes=0, total=1)
+    elif result == 2:  # hard
+      return dict(successes=1, total=1, q0=0.2)
+    elif result == 3:  # good
+      return dict(successes=1, total=1)
+    elif result == 4:  # easy
+      return dict(successes=2, total=2)
+    else:
+      raise Exception('unknown Anki result')
+  elif mode == 'binary':
+    # hard or better is pass
+    return dict(successes=int(result > 1), total=1)
+
+
+def _binomialLogProbability(k: int, n: int, p: float) -> float:
+  assert k <= n
+  return float(binomrv.logpmf(k, n, p))
+
+
+def _noisyLogProbability(result: typing.Union[int, float], q1: float, q0: float, p: float) -> float:
+  z = result >= 0.5
+  return log(((q1 - q0) * p + q0) if z else (q0 - q1) * p + (1 - q0))
+
+
+def printableList(v: list[int | float], more=False) -> str:
+  return ", ".join([f'{x:0.1f}' for x in v]) if not more else ", ".join([f'{x:0.2f}' for x in v])
+
+
+def printDetails(cards, initModels, modelsDb, logLikDb):
+  # key: (card integer, model number, quiz number)
+  for cardNum, card in enumerate(cards):
+    sumLls = [
+        sum([ll
+             for k, ll in logLikDb.items()
+             if k[0] == cardNum and k[1] == modelNum])
+        for modelNum in range(len(initModels))
+    ]
+    print(f'{cardNum}, key={card.key}, lls={printableList(sumLls)}')
+    numQuizzes = len([ll for k, ll in logLikDb.items() if k[0] == cardNum and k[1] == 0])
+
+    lls = []
+    hls = []
+    ps = []
+    for quizNum in range(numQuizzes):
+      lls.append([logLikDb[(cardNum, modelNum, quizNum)] for modelNum in range(len(initModels))])
+      hls.append([
+          ebisu.modelToPercentileDecay(modelsDb[(cardNum, modelNum, quizNum)])
+          for modelNum in range(len(initModels))
+      ])
+    for quizNum, t in enumerate(card.dts_hours):
+      oldModels = initModels if quizNum == 0 else [
+          modelsDb[(cardNum, modelNum, quizNum - 1)] for modelNum in range(len(initModels))
+      ]
+      ps.append([ebisu.predictRecall(m, t) for m in oldModels])
+
+    cumsumLls = np.cumsum(lls, axis=0)
+    for indiv, cumulative, res, t, hl, p in zip(lls, cumsumLls, card.results, card.dts_hours, hls,
+                                                ps):
+      print(
+          f'  {res=}, {t:.1f}h, p={printableList(p, True)}, hl={printableList(hl)}, ll={printableList(indiv)}, cumulative={printableList(cumulative)}'
+      )
+
+
+if __name__ == '__main__':
+  df = utils.sqliteToDf('collection.anki2', True)
+  print(f'loaded SQL data, {len(df)} rows')
+
+  train, TEST_TRAIN = utils.traintest(df, noPerfectCardsInTraining=False)
+  print(f'split flashcards into train/test, {len(train)} cards in train set')
+
+  fracs = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.]
+  cards = [next(t for t in train if t.fractionCorrect >= frac) for frac in fracs]
+  # or
+  cards = train
+
+  initModelParams = [
+      dict(firstHalflife=10, lastHalflife=10e3, firstWeight=0.5),
+      dict(firstHalflife=100, lastHalflife=10e3, firstWeight=0.5),
+      dict(firstHalflife=10, lastHalflife=10e3, firstWeight=0.9),
+      dict(firstHalflife=100, lastHalflife=10e3, firstWeight=0.9),
+  ]
+  initModels = [ebisu.initModel(**args) for args in initModelParams]  #type:ignore
+
+  allModels = dict()  # key: (card integer, model number, quiz number)
+  allLogliks = dict()
+  for cardNum, card in tqdm(enumerate(cards), total=len(cards)):
+    models = initModels
+
+    for quizNum, (ankiResult, elapsedTime) in enumerate(zip(card.results, card.dts_hours)):
+      resultArgs = convertAnkiResultToBinomial(ankiResult, 'approx')
+
+      newModels = []
+      for modelNum, m in enumerate(models):
+        key = (cardNum, modelNum, quizNum)
+
+        newModel = ebisu.updateRecall(m, elapsedTime=elapsedTime, **resultArgs)
+        newModels.append(newModel)
+        allModels[key] = newModel
+
+        if resultArgs['total'] == 1:
+          z = resultArgs['successes'] >= 0.5
+          q1 = max(resultArgs['successes'], 1 - resultArgs['successes'])
+          q0 = resultArgs['q0'] if 'q0' in resultArgs else 1 - q1
+          loglik = _noisyLogProbability(z, q1, q0, ebisu.predictRecall(m, elapsedTime))
+        else:
+          loglik = _binomialLogProbability(resultArgs['successes'], resultArgs['total'],
+                                           ebisu.predictRecall(m, elapsedTime))
+        allLogliks[key] = loglik
+      models = newModels
+
+  # SUMMARY
+  summary = np.zeros((len(cards), len(initModels)))
+  for (cardNum, modelNum, quizNum), ll in allLogliks.items():
+    summary[cardNum, modelNum] += ll
+
+  # DETAILS
+  DETAILS, VIZ = True, True
+  DETAILS, VIZ = False, True
+  if DETAILS:
+    printDetails(cards, models, allModels, allLogliks)
+  if VIZ:
+    plt.figure()
+    plt.plot(np.array(sorted(summary, key=lambda v: v[1])))
+    plt.legend([f'{m}' for m in initModelParams])
+    plt.xlabel('quiz number')
+    plt.ylabel('∑log likelihood')
+    plt.title('Ensemble v3 performance for training set')
---
+++ scripts/utils.py
@@ -0,0 +1,211 @@
+import pandas as pd  # type: ignore
+from dataclasses import dataclass
+from scipy.stats import multinomial  #type:ignore
+import numpy as np
+from typing import Callable, TypeVar
+from collections.abc import Iterable
+import typing
+
+T = TypeVar('T')
+
+
+def weightedMean(w: np.ndarray, x: np.ndarray) -> float:
+  return np.sum(w * x) / np.sum(w)
+
+
+def weightedMeanVar(w: np.ndarray, x: np.ndarray) -> tuple[float, float]:
+  mean = weightedMean(w, x)
+  var = np.sum(w * (x - mean)**2) / np.sum(w)
+  return (mean, var)
+
+
+def meanVarToBeta(mean, var) -> tuple[float, float]:
+  """Fit a Beta distribution to a mean and variance."""
+  # [betaFit] https://en.wikipedia.org/w/index.php?title=Beta_distribution&oldid=774237683#Two_unknown_parameters
+  tmp = mean * (1 - mean) / var - 1
+  alpha = mean * tmp
+  beta = (1 - mean) * tmp
+  return alpha, beta
+
+
+def clampLerpFloat(x1: float, x2: float, y1: float, y2: float, x: float):
+  mu = (x - x1) / (x2 - x1)  # will be >=0 and <=1
+  # branchless: hoping it's faster (cache misses, etc.) than the equivalent:
+  # `y1 if (x < x1) else y2 if (x > x2) else (y1 * (1 - mu) + y2 * mu)`
+  return (x < x1) * y1 + (x > x2) * y2 + (x1 <= x <= x2) * (y1 * (1 - mu) + y2 * mu)
+
+
+def sequentialImportanceResample(particles: np.ndarray,
+                                 weights: np.ndarray,
+                                 N=None) -> tuple[np.ndarray, np.ndarray]:
+  if N is None:
+    N = len(particles)
+  draw: np.ndarray = multinomial.rvs(N, weights / np.sum(weights))
+  # each element of `draw` is an integer, the number of times the particle at that index should appear in the output
+
+  # this isn't going to be fast FIXME
+  newParticles = np.hstack(
+      [np.ones(repeat) * particle for repeat, particle in zip(draw, particles)])
+  newWeights = np.ones(N)
+  return (newParticles, newWeights)
+
+
+def split_by(split_pred: Callable[[T, list[T]], bool], lst: Iterable[T]) -> list[list[T]]:
+  "Allows each element to decide if it wants to not be in previous partition"
+  lst = iter(lst)
+  try:
+    x = next(lst)
+  except StopIteration:  # empty iterable (list, zip, etc.)
+    return []
+  ret: list[list[T]] = []
+  ret.append([x])
+  for x in lst:
+    if split_pred(x, ret[-1]):
+      ret.append([x])
+    else:
+      ret[-1].append(x)
+  return ret
+
+
+def partition_by(f: Callable[[T], bool], lst: Iterable[T]) -> list[list[T]]:
+  "See https://clojuredocs.org/clojure.core/partition-by"
+  lst = iter(lst)
+  try:
+    x = next(lst)
+  except StopIteration:  # empty iterable (list, zip, etc.)
+    return []
+  ret: list[list[T]] = []
+  ret.append([x])
+  y = f(x)
+  for x in lst:
+    newy = f(x)
+    if y == newy:
+      ret[-1].append(x)
+    else:
+      ret.append([x])
+    y = newy
+  return ret
+
+
+def traintest(inputDf, minQuiz=5, minFractionCorrect=0.67, noPerfectCardsInTraining=True):
+  """Split an Anki Pandas dataframe of all reviews into a train set and a test set
+  
+  Groups all reviews into those belonging to the same card. Throws out cards with too few reviews
+  or too few correct reviews. Splits the resulting cards into a small train set and a larger test
+  set. We'll only look at the training set when designing our new update algorithm.
+
+  The training set will only have cards with less than perfect reviews (since it's impossible for
+  a meaningful likelihood to be computed for all passing reviews).
+  """
+  allGroups: list[Card] = []
+  for key, df in inputDf.groupby('cid'):
+    if len(df) < minQuiz:
+      continue
+
+    # "Group chunks should be treated as immutable, and changes to a group chunk may produce
+    # unexpected results"
+    # https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#transformation
+    sortedDf = df.copy().sort_values('timestamp')
+
+    dts_hours, results, absts_hours = dfToVariables(sortedDf)
+    if len(results) < minQuiz:
+      continue
+
+    fractionCorrect = np.mean(np.array(results) > 1)
+    if fractionCorrect < minFractionCorrect:
+      continue
+
+    allGroups.append(
+        Card(
+            df=sortedDf,
+            len=len(results),
+            key=key,
+            fractionCorrect=fractionCorrect,
+            dts_hours=dts_hours,
+            results=results,
+            absts_hours=absts_hours,
+        ))
+  allGroups.sort(key=lambda d: d.fractionCorrect)
+  trainGroups = [
+      group for group in allGroups[::3]
+      if group.fractionCorrect < (1.0 if noPerfectCardsInTraining else 1.1)
+  ]
+  return trainGroups, allGroups
+
+
+def sqliteToDf(filename: str, reviewsWithCardsOnly=True):
+  sqlReviewsWithCards = 'select revlog.*, notes.flds from revlog inner join notes on revlog.cid = notes.id'
+  sqlAllReviews = sqlReviewsWithCards.replace(' inner join ', ' left outer join ')
+  SQL_TO_USE = sqlReviewsWithCards if reviewsWithCardsOnly else sqlAllReviews
+  # Sometimes you delete cards because you were testing reviews, or something?
+  # So here you might want to just look at reviews for which the matching cards
+  # still exist in the deck.
+
+  import sqlite3
+  con = sqlite3.connect(filename)
+  df = pd.read_sql(SQL_TO_USE, con)
+  con.close()
+  df['timestamp'] = df.id.astype('datetime64[ms]')
+  return df
+
+
+def dfToVariables(g):
+  """Convert a Pandas dataframe of an Anki SQLite database to a list of delta-times and results
+
+  Given a dataframe containing all reviews of a single card, we want to get a simple list of hours
+  before each review as well as the result of that review (1=fail, 2=hard, 3=normal, 4=easy).
+
+  This function does some extra work to find successive failed reviews and combine them into a
+  SINGLE failed review if they happened close enough. This is because sometimes in my data, I have
+  a failure, and then Anki quizzed me a couple of minutes ago and I accidentally clicked "fail"
+  again. This erroneous data can really mess up Ebisu so I want to try and combine these runs of
+  two or more successive failures that happen within an interval (say, a half-hour), into a single
+  failure.
+  """
+  assert g.timestamp.is_monotonic_increasing
+  hour_per_millisecond = 1 / 3600e3
+  # 1-4: results of quiz
+  results: list[int] = g.ease.values
+  # absolute time of this quiz
+  ts_hours: list[float] = g.timestamp.values.astype('datetime64[ms]').astype(
+      'timedelta64[ms]').astype(float) * hour_per_millisecond
+
+  ret = []
+  HOUR_WINDOW = 4.0
+  for group in split_by(lambda rev, revs: (rev[1] - revs[-1][1]) > HOUR_WINDOW,
+                        zip(results, ts_hours)):
+    # each `group` contains a list of reviews that all happened in the same HOUR_WINDOW window.
+    # Anki only does daily-ish reviews so reviews this close together must have happened due to a failure.
+    # We want to clean up those.
+
+    # See https://stackoverflow.com/a/8534381
+    failure = next(((result, t) for result, t in group if result == 1), None)
+
+    if failure:
+      # In general, this group will be [fail, pass] or maybe [pass, fail, pass] (if I accidentally clicked "pass" at first).
+      # Add only the first failure in this HOUR_WINDOW-group of reviews
+      ret.append(failure)
+    else:
+      # add all reviews in this HOUR_WINDOW-group because all were successes.
+      # In reality this group will most likely be 1-element long
+      ret.extend(group)
+
+  results, ts_hours = zip(*ret)
+  # drop the first quiz (that's our "learning" case)
+  # delta time between this quiz and the previous quiz/study
+  dts_hours = np.diff(ts_hours).tolist()
+  ts_hours = ts_hours[1:]
+  results = results[1:]
+
+  return dts_hours, results, ts_hours
+
+
+@dataclass
+class Card:
+  df: pd.DataFrame
+  len: int
+  key: int
+  fractionCorrect: float
+  dts_hours: list[float]
+  results: list[int]
+  absts_hours: list[float]
